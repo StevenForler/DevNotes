31296a4566ff287be4f4f08c807e271d42827c1c
StevenForler <stevensjsf@gmail.com>
2024-08-26 21:20:36 -0700

    completed todo list video
    NEEDS DELETE BUTTON PER LIST ITEM

--- Test_todo_list/script.js
+++ Test_todo_list/script.js
@@ -1,11 +1,11 @@
-const items_container = document.getElementById("items");
-const item_template = document.getElementById("itemTemplate");
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
 const ADD_BUTTON = document.getElementById("add");
 
 let items = getItems();
 
 function getItems() {
-    const value = localStorage.getItem("todo")|| "[]";
+    const value = localStorage.getItem("todo") || "[]";
 
     return JSON.parse(value); // to turn the JSON array into a JavaScript array
 }
@@ -26,15 +26,51 @@ function addItem() {
     refreshList(); // render the list of items for the user
 }
 
-function refreshList() {
-    //TODO: sort items
+function updateItem(item, key, value) {
+    item[key] = value; // JS objects are passed by reference so updating the item here should update the same object
 
-    items_containers.innerHTML = "";
+    setItems(items);
+    refreshList();
+}
+
+function refreshList() {
+    // TODO item sort that handles alphabet org and if its checked or not.
+    // items.sort((a,b) => {
+    //     if (a.completed) {
+    //         return 1;
+    //     }
+
+    //     if (b.completed) {
+    //         return -1;
+    //     }
+
+    //     return a.description < b.description ? -1 : 1;
+    // })
+
+    ITEMS_CONTAINER.innerHTML = "";
 
     for (const item of items) {
-        const itemElement = item_template.content.cloneNode(true); // were taking the template element on line 13 of index.html, then were getting the content from that template, then make a clone of that template. now we can interact with that div using standard DOM methods
-        
+        const itemElement = ITEM_TEMPLATE.content.cloneNode(true); // were taking the template element on line 13 of index.html, then were getting the content from that template, then make a clone of that template. now we can interact with that div using standard DOM methods
+        const descriptionInput = itemElement.querySelector(".item-description"); // for every single new item we are cloning that template.
+        const completedInput = itemElement.querySelector(".item-completed");
+
+        descriptionInput.value = item.description;
+        completedInput.checked = item.completed;
+
+        descriptionInput.addEventListener("change", () => {
+            updateItem(item, "description", descriptionInput.value);
+        });
+
+        completedInput.addEventListener("change", () => {
+            updateItem(item, "completed", completedInput.checked);
+        });
+
+        ITEMS_CONTAINER.append(itemElement);
     }
 }
-console.log(items);
-// getItems();
+
+ADD_BUTTON.addEventListener ("click", () => {
+    addItem();
+});
+
+refreshList();
