fae1703c958b7648e6ee9ac355ec646bbeb8377e
StevenForler <stevensjsf@gmail.com>
2024-09-06 15:34:02 -0700

    updated notes

--- Javascript_notes/beginner_projects/Palindrome_Checker/script.js
+++ Javascript_notes/beginner_projects/Palindrome_Checker/script.js
@@ -1,11 +1,13 @@
 const input = document.getElementById("input") // when we have the element it gives us access to the whole box. we can change the styling, clear the box. etc.
 
 function reverseString(str) {
-    
+
+    return str.split("") // it takes the string and splits it based on the delimeter( which is the value that you put in as the argument in the function) for example by default when using an empty string it just takes each letter separately as an array.
+
 }
 
 function check(){
     const value= input.value;
 
-    alert(value)
+    // alert(value)
 }
