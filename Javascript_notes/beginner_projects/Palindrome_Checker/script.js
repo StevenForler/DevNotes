f8d201b80fceec518522c1708d1db1dc4f72a86e
StevenForler <stevensjsf@gmail.com>
2024-10-07 23:23:43 -0700

    finished palindrome checker

--- Javascript_notes/beginner_projects/Palindrome_Checker/script.js
+++ Javascript_notes/beginner_projects/Palindrome_Checker/script.js
@@ -2,12 +2,19 @@ const input = document.getElementById("input") // when we have the element it gi
 
 function reverseString(str) {
 
-    return str.split("") // it takes the string and splits it based on the delimeter( which is the value that you put in as the argument in the function) for example by default when using an empty string it just takes each letter separately as an array.
+    return str.split("").reverse().join("") // it takes the string and splits it based on the delimeter( which is the value that you put in as the argument in the function) for example by default when using an empty string it just takes each letter separately as an array.
 
 }
 
-function check(){
+function check() {
     const value= input.value;
-
-    // alert(value)
+    const reverse = reverseString(value)
+    
+    if (value === reverse) {
+        alert("This is a Palindrome!")
+    } else {
+        alert("no no no, not today!")
+    }
+    
+    input.value = ""
 }
