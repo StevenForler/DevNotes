3481ebff85cd1330bc702d0cabeff0607bb5c64e
StevenForler <stevensjsf@gmail.com>
2024-11-05 23:23:19 -0800

    finished color flipper

--- Javascript_notes/beginner_projects/Code_with_bubb/Color_Flipper/script.js
+++ Javascript_notes/beginner_projects/Code_with_bubb/Color_Flipper/script.js
@@ -1,10 +1,35 @@
-const newColorBtnElement = document.getElementById("new-color-button")
-const currentColorElement = document.getElementById("current-color")
+const newColorBtnElement = document.getElementById('new-color-button');
 
-const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] //fuck doing it this way. find a better way to do this in the future.
+const currentColorElement = document.getElementById('current-color');
+
+const hexValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',]; //fuck doing it this way. find a better way to do this in the future.
 
 function getRandomHexValue() {
     const randomIndexPosition = Math.floor(
         Math.random() * hexValues.length
     );
+
+    const randomHexValue = hexValues[randomIndexPosition];
+
+    return randomHexValue;
 }
+
+function getRandomHexString(stringLength){
+    let hexString = '';
+    for (let i = 0; i < stringLength; i++){
+        hexString += getRandomHexValue();
+    }
+
+    return hexString;
+}
+
+newColorBtnElement.addEventListener("click", () => {
+    const randomHexString = '#' + getRandomHexString(6);
+
+    document.body.style.setProperty(
+        'background-color',
+        randomHexString
+    );
+    
+    currentColorElement.textContent = randomHexString;
+});
