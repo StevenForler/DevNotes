74e786a6aeb423c317c9c1e9571e79a49ad5278e
StevenForler <stevensjsf@gmail.com>
2025-07-31 22:36:55 -0700

    usestatenotes

--- react_practice_projects/smoljames-projects/todo-list/src/components/TodoInput.jsx
+++ react_practice_projects/smoljames-projects/todo-list/src/components/TodoInput.jsx
@@ -8,6 +8,9 @@ export function TodoInput(props) {
         <div className="input-container">
             <input value={inputValue} onChange={(e) => {
                 setInputValue(e.target.value)
+                {/* the input value will take from the inputValue set on line 9
+                    and with setInputValue set to pase th target value, it shall pass through the use stake hook
+                    as a string within the default array set earlier*/}
             }} placeholder="Add task" />
             <button onClick={() => {
                 if (!inputValue) { return }
