aadc716289bdd0e3642bf13144503725faa76c3c
StevenForler <stevensjsf@gmail.com>
2025-08-12 23:19:24 -0700

    more-backend-coding-notes

--- Backend_Notes/node.js/Smoljames-project/Chapter2/server.js
+++ Backend_Notes/node.js/Smoljames-project/Chapter2/server.js
@@ -7,22 +7,41 @@ const PORT = 8383 // referenced to have the variable name in all caps. pick a 4 
 
 
 {/* the core requirements of getting a backend server up and running is very minimal
-    granted it does nothing but after telling node in the terminal to run it. "node server.js" 
-    it will start listenting to network requests until you manual close it
+granted it does nothing but after telling node in the terminal to run it. "node server.js" 
+it will start listenting to network requests until you manual close it
 const express = require('express')
 const app = express() 
 const PORT = (look into on how to find port number)
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`)) */}
+
+// * Ctrl+C in terminal kills the program. When there are programs that are indefinitely running you need to manually kill them.
+// * npm run dev is a script in the package.json file that you can manipulate. upon initial creation "npm run dev" is not in there.
+/**
+**  "scripts": {
+**  "dev" : "node server.js", 
+**  "test": "echo \"Error: no test specified\" && exit 1"
+  },
+ */
 
 let data = ['james']
 
 // Middleware
 app.use(express.json())
 
+/**  
+** At base the server isn't ready to handle incomming requests or interpret incoming requests.
+** Below in James' notes talks about some of the things that need setup to handle incoming requests
+** VERBs(methods) are what help the server communicate with the client side. Paths are just additions to the home page of your weblink that lead to other parts of your page.
+*/
 // ENDPOINT - HTTP VERBS (method) && Routes (or paths)
 // The method informs the nature of request and the route is a further subdirectory (basically we direct the request to the body of code to respond appropriately, and these locations or routes are called endpoints)
 
 // Type 1 - Website endpoints (these endpoints are for sending back html and they typically come when a user enters a url in a browser)
+
+/**
+ ** how to structure the app.get method to get the home route:
+ *? app.get('/', ()=>)
+ */
 
 app.get('/', (req, res) => {
     console.log('User requested the home page website')
