cbd7bf1f515c0d88c868283d171316f79de2b77a
StevenForler <stevensjsf@gmail.com>
2025-09-28 23:53:46 -0700

    notesapp-start

--- linktreeclone-fs/components/gif.jsx
+++
@@ -1,7 +0,0 @@
-export function Cloud() {
-    return(
-            <img className="w-36 h-24" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a67ac30c-fe21-4e97-8dad-ffa8b2670167/dd722kz-6af5fd2a-fee4-4fcd-994a-73b884b8a7da.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi9hNjdhYzMwYy1mZTIxLTRlOTctOGRhZC1mZmE4YjI2NzAxNjcvZGQ3MjJrei02YWY1ZmQyYS1mZWU0LTRmY2QtOTk0YS03M2I4ODRiOGE3ZGEuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.qGqc4npOGmnh2Gm-ZB9MrbuHlckdMhVIsyDpS8sIruY" >
-                
-            </img>
-    )
-}
--- linktreeclone-fs/components/placeholder.jsx
+++
@@ -1,5 +0,0 @@
-export function Placeholder(){
-    return(
-        <img src="https://placehold.co/640x480" className="w-52 rounded-3xl"  ></img>
-    )
-}
