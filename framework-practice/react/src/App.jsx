71ec265cd2296d8e894021a1657e3c93317fcc33
StevenForler <48844649+StevenForler@users.noreply.github.com>
2025-10-10 12:58:52 -0700

    Framework-practice-playground-initialization

---
+++ framework-practice/react/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="text-9xl">
+      <p>test sentence to see if tailwind is working</p>
+    </div>
+    </>
+  )
+}
+
+export default App
