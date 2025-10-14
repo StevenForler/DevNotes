5b18dcefe9ec16b8ff49bf260cc469c575a7caae
StevenForler <stevensjsf@gmail.com>
2025-10-13 23:12:17 -0700

    New component-Scope.md-added

---
+++ framework-practice/react/Scope.md
@@ -0,0 +1,4 @@
+# next update ideas
+
+## theme changer
+- use different varieties of pokeball colors to give different themes for the page
---
+++ framework-practice/react/components/infoCard.jsx
@@ -0,0 +1,16 @@
+export default function InfoCard(){
+    const info = [
+        {
+            color: "bg-red-500",
+            text: "Resources"
+        },
+        {
+            color: "bg-cyan-600",
+            text: "Pokemon inspired games"
+        },
+        {
+            color: "bg-zinc-100",
+            text: "Romhacks"
+        },
+    ]
+}
