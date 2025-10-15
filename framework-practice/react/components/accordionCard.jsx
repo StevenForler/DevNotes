2320faafce79cf2d081d396a76a3534f34fe1551
StevenForler <48844649+StevenForler@users.noreply.github.com>
2025-10-15 12:35:47 -0700

    accordioncard-created

---
+++ framework-practice/react/components/accordionCard.jsx
@@ -0,0 +1,8 @@
+
+export default function accordionCard() {
+    return (
+        <div>
+            <>{/*Do more research on finding a tutorial that allows me to adapt the CSS of the dropdown*/}</>
+        </div>
+    )
+}
