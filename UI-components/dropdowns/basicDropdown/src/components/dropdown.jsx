fd9808356c66dcadc143761af0db30b9398d5584
StevenForler <stevensjsf@gmail.com>
2025-12-02 01:08:17 -0800

    fixed the drop down to sit inside 1 div.

--- UI-components/dropdowns/basicDropdown/src/components/dropdown.jsx
+++ UI-components/dropdowns/basicDropdown/src/components/dropdown.jsx
@@ -2,9 +2,8 @@ import {useEffect, useRef, useState} from "react";
 
 const Dropdown = () => {
     const [isOpen, setIsOpen] = useState(false)
-
     const dropdownRef = useRef(null);
-    useEffect (() => { // revisit in video explaining this useEffect and useRef in detail
+    useEffect (() => {
         const handleClickOutside = (e) => {
             if(!dropdownRef.current?.Contains(e.target)) {
                 setIsOpen(false);
@@ -16,22 +15,14 @@ const Dropdown = () => {
         }
 
         return () => document.removeEventListener("mousedown", handleClickOutside);
-    }, [isOpen])
-    /* this is half working. Currently only when I select any links cause the dropdown to close.
-    This might be due to how I separating the drop box into its own div to control how the line up works
-     2 options here and I'm inclined to do both as a learning opportunity
-     1. rework it to work with the menuItems in the same div.
-     2. get it to work as is but this might require finding a new video to follow an example of
-     ... or maybe get some things on this from Johanna.*/
+    }, [isOpen]);
 
     const menuItems = ["Home", "Settings", "Logout"]
     return (
-        <div>
-            <div ref={dropdownRef} className="main-container">
+            <div ref={dropdownRef} className="dropdown-container">
                 <button onClick={() => setIsOpen((prev) => !prev)}>
-                    here's a button
+                    <span> here's a button </span>
                 </button>
-                <div className="dropdown-container">
                     {isOpen && (
                     <div id="dropdown-info">
                         {menuItems.map((item) =>(
@@ -40,21 +31,8 @@ const Dropdown = () => {
                             </a>
                         ))}
                     </div>
-                    )}
-                </div>
-                {/* <div className="dropdown-container">
-                    {isOpen && (
-                    <div id="dropdown-info">
-                        {menuItems.map((item) =>(
-                            <a  key={item} href={item}> 
-                                {item}
-                            </a>
-                        ))}
-                    </div>
-                    )}
-                </div> */}
+                )}
             </div>
-        </div>
     )
 }
 
